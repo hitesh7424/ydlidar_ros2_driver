77a0b8118b8b1ed5d70646dac654e775c9a76d62
Hitesh <hitesh164722@gmail.com>
2026-05-20 08:21:09 +0530

    fix: use RELIABLE QoS for /scan publisher to fix rviz2 compatibility
    Changed SensorDataQoS() to SystemDefaultsQoS() on /scan topic
    publisher to resolve RELIABILITY_QOS_POLICY mismatch.

--- src/ydlidar_ros2_driver_node.cpp
+++ src/ydlidar_ros2_driver_node.cpp
@@ -184,7 +184,7 @@ int main(int argc, char *argv[]) {
     RCLCPP_ERROR(node->get_logger(), "%s\n", laser.DescribeError());
   }
   
-  auto laser_pub = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::SensorDataQoS());
+  auto laser_pub = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::SystemDefaultsQoS());
 
   auto stop_scan_service =
     [&laser](const std::shared_ptr<rmw_request_id_t> request_header,
